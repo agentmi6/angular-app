e3a67307d487029446f3ccdf95efa87571be7d3e
agentmi6 <agentmi6@gmail.com>
2018-05-11 21:25:02 +0200

    21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {HttpModule} from '@angular/http';
 import {ServerComponent} from './server/server.component';
-
+import {HeaderComponent} from './header/header.componenet';
+import {FooterComponent} from './footer/footer.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    HeaderComponent,
+    FooterComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
